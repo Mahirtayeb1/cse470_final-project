63ed2b6e1cef54a6c2fc4dc5bd94a9d925ea6576
MahirFaisal <mahirtayeb1@gmail.com>
2023-05-12 07:08:19 +0600

    testing home app

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class HomeViewsTestCase(TestCase):
+    def test_index_view(self):
+
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
